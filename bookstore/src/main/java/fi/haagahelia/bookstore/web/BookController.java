3acdd75f4004ce0f2ea161af047b3a98dd8b9e0e
Samirio-o <samirio.skrt@gmail.com>
2026-09-09 16:40:41 +0300

    Implemented BookController and BookRepository connection, added booklist.html

--- bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,14 +1,27 @@
 package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import fi.haagahelia.bookstore.repository.BookRepository;
+
 
 @Controller
 public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping("/index")
     public String index() {
         return "index";
     }
 
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
 }
